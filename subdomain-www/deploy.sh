5fb7356219aebe3764cbff9c78d274fb31ea60ef
sebastian <sebastian@sommerfeld.io>
2022-05-03 15:56:11 +0200

    docs: include firefox bookmarks export in website

--- subdomain-www/deploy.sh
+++ subdomain-www/deploy.sh
@@ -15,8 +15,20 @@
 # The script does not accept any parameters.
 
 
+case $HOSTNAME in
+("pegasus") echo -e "$LOG_INFO Script running on expected machine $HOSTNAME";;
+(*)         echo -e "$LOG_ERROR SCRIPT NOT RUNNING ON EXPECTED MACHINE !!!" && echo -e "$LOG_ERROR Exit" && exit 0;;
+esac
+
+
 SRC_DOCKER_IMAGE="pegasus/website:dev"
 TARGET_DOCKER_IMAGE="sommerfeldio/website"
+
+FTP_HOST="w00f8074.kasserver.com"
+FTP_USER_FILE="resources/.secrets/ftp.user"
+FTP_PASS_FILE="resources/.secrets/ftp.pass"
+CONTENT_DIR="src/main"
+
 
 echo -e "$LOG_INFO Deploy image $TARGET_DOCKER_IMAGE to DockerHub"
 
@@ -41,4 +53,24 @@ docker pull "$TARGET_DOCKER_IMAGE:latest"
 
 echo -e "$LOG_DONE Finished deployment of $TARGET_DOCKER_IMAGE"
 
-# todo ... ftp upload ... similar to other static websites
+echo -e "$LOG_INFO FTP upload"
+
+echo -e "$LOG_INFO Read FTP user and password from $FTP_USER_FILE and $FTP_PASS_FILE"
+FTP_USER=$(cat "$FTP_USER_FILE")
+FTP_PASS=$(cat "$FTP_PASS_FILE")
+
+if [[ ! -d "$CONTENT_DIR" ]]
+then
+  echo -e "$LOG_ERROR Directory '$CONTENT_DIR' missing -> No files to upload"
+  echo -e "$LOG_ERROR exit"
+  exit 0
+fi
+
+echo -e "$LOG_INFO Deploy files to webspace via FTP"
+(
+  cd "$CONTENT_DIR" || exit
+  # shellcheck disable=SC2035
+  docker run -it --rm --volume "$(pwd):$(pwd)" --workdir "$(pwd)" pegasus/ftp-client:latest ncftpput -R -v -u "$FTP_USER" -p "$FTP_PASS" "$FTP_HOST" / *
+)
+
+echo -e "$LOG_DONE FTP upload"
