22cf7c2b69a687142ba5284bdea114a85f9429e5
sebastian <sebastian@sommerfeld.io>
2022-05-03 14:59:55 +0200

    feat: big linter update -> added jsonlint, shellcheck for all *.sh files, find Dockerfiles automatically, lint terraform files, replaced folderslint with directoryvalidator

--- lint.sh
+++ lint.sh
@@ -11,9 +11,9 @@
 
 echo -e "$LOG_INFO Download latest linter definitions"
 linterDefinitions=(
+  '.directoryvalidator.json'
   '.ls-lint.yml'
-  '.yamllint'
-  '.folderslintrc'
+  '.yamllint.yml'
 )
 for file in "${linterDefinitions[@]}"; do
   rm "$file"
@@ -22,11 +22,37 @@ for file in "${linterDefinitions[@]}"; do
 done
 
 echo -e "$LOG_INFO ------------------------------------------------------------------------"
+
 echo -e "$LOG_INFO Run linter containers"
+
+echo -e "$LOG_INFO check mandatory files"
+docker run -it --rm --volume "$(pwd):$(pwd)" --workdir "$(pwd)" pegasus/directory-validator:latest directory-validator .
+
+echo -e "$LOG_INFO yamllint"
 docker run -it --rm --volume "$(pwd):/data" --workdir "/data" cytopia/yamllint:latest .
-docker run -it --rm --volume "$(pwd):/data" --workdir "/data" koalaman/shellcheck:latest ./*.sh
-docker run -i  --rm hadolint/hadolint < subdomain-docs/Dockerfile
-docker run -i  --rm hadolint/hadolint < subdomain-www/Dockerfile
+
+echo -e "$LOG_INFO jsonlint"
+docker run -it --rm --volume "$(pwd):/data" --workdir "/data" cytopia/jsonlint:latest -i '*node_modules*' "*.json"
+
+echo -e "$LOG_INFO shellcheck"
+find . -not \( -path "*node_modules*" \) -name "*.sh" -exec docker run -it --rm --volume "$(pwd):/data" --workdir "/data" koalaman/shellcheck:latest {} \;
+
+echo -e "$LOG_INFO lint Dockerfile"
+find . -not \( -path "*node_modules*" \) -name Dockerfile -exec \
+    sh -c 'src=${1#./} && echo "$LOG_INFO Lint $1" && docker run -i  --rm hadolint/hadolint < $1' sh "{}" \;
+
+echo -e "$LOG_INFO lint Vagrantfile"
+# todo ...
+
+echo -e "$LOG_INFO lslint"
 docker run -it --rm --volume "$(pwd):/data" --workdir "/data" lslintorg/ls-lint:1.11.0
-docker run -it --rm --volume "$(pwd):$(pwd)" --workdir "$(pwd)" pegasus/folderslint:latest folderslint .
+
+echo -e "$LOG_INFO lint .env files"
+docker run -it --rm --volume "$(pwd):/app" --workdir "/app" dotenvlinter/dotenv-linter:latest --exclude "*node_modules*" --recursive
+
+echo -e "$LOG_INFO lint terraform files"
+find . -not \( -path "*node_modules*" \) -name "*.tf" -exec docker run -it --rm --volume "$(pwd):$(pwd)" --workdir "$(pwd)" ghcr.io/terraform-linters/tflint:latest {} \;
+
 echo -e "$LOG_INFO ------------------------------------------------------------------------"
+
+echo -e "$LOG_DONE Finished linting"
